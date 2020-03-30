c76af14bfb62c6ff2dfe5ecf16d3d6b3c78cf927
Maria Jose <mj.ruizg1@uniandes.edu.co>
2020-03-30 18:23:09 -0500

    Taller Completo

---
+++ dataCourses.ts
@@ -0,0 +1,10 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course(" Desarrollo de SW", "César Augusto Forero", 4),
+  new Course("Constitución", "Natalia Angel Cabo", 2),
+  new Course("Anadec", "Miguel Angel García", 2),
+  new Course("Escritura II", "Nelsy Cristina Lopez", 2),
+  new Course("Arquiem", "Maria Camila Romero", 3),
+  new Course("Infratec", "Rafael Gomez", 3)
+]
---
+++ scripts/dataCourses.js
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course(" Desarrollo de SW", "César Augusto Forero", 4),
+    new Course("Constitución", "Natalia Angel Cabo", 2),
+    new Course("Anadec", "Miguel Angel García", 2),
+    new Course("Escritura II", "Nelsy Cristina Lopez", 2),
+    new Course("Arquiem", "Maria Camila Romero", 3),
+    new Course("Infratec", "Rafael Gomez", 3)
+];
